bb5e82f43eb6f5081d372749523a2fcc438d7990
Makhliw <abdumutalovamakhliyokhon@gmail.com>
2025-10-25 12:23:58 +0500

    Delete lesson-1/homework/homwork1-task3.py

--- lesson-1/homework/homwork1-task3.py
+++
@@ -1,6 +0,0 @@
-#Lesson 1 . Homework
-#task2-Given diameter of circle. Find its length.
-a = float(input("Enter first number: "))
-b = float(input("Enter second number: "))
-mean = (a + b) / 2
-print("Mean:", mean)
