332cfbebeadf83b505bd4906fd9f7847ad151044
Makhliw <abdumutalovamakhliyokhon@gmail.com>
2025-10-25 12:24:46 +0500

    Delete lesson-1/homework/homework-1- task 1.py

--- lesson-1/homework/homework-1- task 1.py
+++
@@ -1,7 +0,0 @@
-#Lesson 1 . Homework
-#Task3 -Given two numbers a and b. Find their mean.
-side = float(input("Enter side of square: "))
-perimeter = 4 * side
-area = side ** 2
-print("Perimeter:", perimeter)
-print("Area:", area)
