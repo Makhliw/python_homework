da3be42e65d269ac23e80ea94727d1ebe4f79101
Makhliw <abdumutalovamakhliyokhon@gmail.com>
2025-10-25 12:24:23 +0500

    Delete lesson-1/homework/homework-1- task 2.py

--- lesson-1/homework/homework-1- task 2.py
+++
@@ -1,6 +0,0 @@
-#Lesson 1 . Homework
-#task2-Given diameter of circle. Find its length.
-import math
-diameter = float(input("Enter diameter of circle: "))
-circumference = math.pi * diameter
-print("Circumference:", circumference)
